af031cc28a255156685a427b50c08946c1f2a8a2
Quartermaster-90 <christopher.belsey@icloud.com>
2023-06-01 01:49:22 +0100

    Gave up

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -3,3 +3,35 @@ def return_10():
 
 def add(first_num, second_num):
     return first_num + second_num
+
+def subtract(first_num, second_num):
+    return first_num - second_num
+
+def multiply(first_num, second_num):
+    return first_num * second_num
+
+def divide(first_num, second_num):
+    return first_num / second_num
+
+def length_of_string(cheese): # cheese being the 'test_string' variable
+    return len(cheese)
+
+def join_string(sentence_one, sentence_two):
+    return sentence_one + sentence_two
+
+# def add_string_as_number(num_one, num_two):
+#     return int(num_one + num_two)
+
+def number_to_full_month_name(months):
+    for month in months:
+        if month == {1: "January", 3: "March", 9:"September"}:
+            return months
+
+# def number_to_full_month_name(month_three):
+#     month_three = "March"
+#     return month_three
+
+# def number_to_full_month_name(month_nine):
+#     month_nine = "September"
+#     return month_nine
+
